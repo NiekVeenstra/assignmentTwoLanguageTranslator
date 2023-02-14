670711a9747ee5418001e7289c16eb93435f755d
“Niek” <n.veenstra02@gmail.com>
2023-02-14 19:54:56 +0100

    setup the routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import StartupPage from "./pages/StartupPage";
+import TranslationPage from "./pages/TranslationPage";
+import ProfilePage from "./pages/ProfilePage";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<StartupPage/>} />
+      <Route path="/translation" element={<TranslationPage/>}/>
+      <Route path="/profile" element={<ProfilePage/>}/>
+    </Routes>
+  );
 };
 
 export default App;
--- src/pages/TranslationPage.jsx
+++ src/pages/TranslationPage.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const TranslationPage = () => {
   return <div>TranslationPage</div>;
 };
